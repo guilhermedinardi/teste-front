18268e39836c2075591badd3080029307e4a5e5c
Guilherme Dinardi <gdinardito@gmail.com>
2020-11-12 22:17:44 -0300

    api

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 //Consumes fake API data like Get, Post and Delete
 const api = axios.create({
-    baseURL: 'https://apifakev4.herokuapp.com/'
+    baseURL: 'https://fakeapitestv4.herokuapp.com/'
 })
 
 export default api;
