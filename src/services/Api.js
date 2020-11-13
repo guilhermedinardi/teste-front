049a26060568d7227629431e393ba6360d14a04c
Guilherme Dinardi <gdinardito@gmail.com>
2020-11-12 21:24:58 -0300

    Integrated API

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 //Consumes fake API data like Get, Post and Delete
 const api = axios.create({
-    baseURL: 'https://test-v4-front.herokuapp.com/'
+    baseURL: 'https://apifakev4.herokuapp.com/'
 })
 
 export default api;
